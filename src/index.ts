ac02d00081c3e7e7091fc1c4c7fe443b0b2b6899
Rohit Kumar Barada <rohitbarada@gmail.com>
2025-09-15 02:14:58 +0530

    nothing

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ const wss = new WebSocketServer({ server: httpServer });
     wss.on('connection', function connection(ws) {
     wss.on('error', console.error);
     console.log("user count",++count);
+    
   ws.on('message', function message(data, isBinary) {
     wss.clients.forEach(function each(client) {
       if (client.readyState === WebSocket.OPEN) {
